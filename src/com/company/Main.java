044eea909bbf515bd555c0d9c41c1a5b73bbe983
navin <navin.kumar.18@mountblue.tech>
2021-11-16 12:29:45 +0530

    Reading deliveris.csv File data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            File file = new File("/home/navin/Projects/ipl-dataset-proj/src/com/company/dataset/deliveries.csv");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("File not found -> " + e.getMessage());
+        }
     }
 }
